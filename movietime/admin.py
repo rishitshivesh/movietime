897c413981021fc7fcab4a26342432c6127747ea
Rishit Shivesh <rishitshivesh@gmail.com>
2020-10-27 22:25:34 +0530

    Admin search

--- movietime/admin.py
+++ movietime/admin.py
@@ -7,8 +7,8 @@ class UserDisplay(admin.ModelAdmin):
     list_display = ("id","first_name","last_name","username","email")
 class BookingDisplay(admin.ModelAdmin):
     list_display = ("id","movieid","user","BookingTime","SelectedTime","SelectedDate","cancelled")
-    list_filter = ("user","SelectedTime",('BookingTime', DateTimeRangeFilter),"cancelled")
-    search_fields = ['user','SelectedTime','SelectedDate']
+    list_filter = ("SelectedTime",('BookingTime', DateTimeRangeFilter),"cancelled")
+    search_fields = ('user','SelectedTime','SelectedDate',"movieid","moviename")
 admin.site.register(User,UserDisplay)
 admin.site.register(Booking,BookingDisplay)
 
