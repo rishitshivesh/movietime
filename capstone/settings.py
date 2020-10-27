0a9a1648fdd541ed0cfb74f6e0879cb27d7b3279
Rishit Shivesh <rishitshivesh@gmail.com>
2020-10-27 22:55:52 +0530

    Adminpage beautify debug

--- capstone/settings.py
+++ capstone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+d4=yy60d4nm6)(t6%2bz&anxg3@)hb$-!y*6!u-z6$%pl(z)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','0.0.0.0']
 
